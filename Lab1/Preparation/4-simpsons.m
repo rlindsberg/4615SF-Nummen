f7e10a0ec3da94685454537b909d6792a78285db
rlindsberg <roderick@karlemstrand.com>
2021-01-27 23:11:40 +0100

    Finish pre 4

--- Lab1/Preparation/4-simpsons.m
+++ Lab1/Preparation/4-simpsons.m
@@ -1,20 +1,34 @@
-f = @(x) cos(x.^2);
-a = 0; % start_value = 0;
-b = 1; % stop_value = 1;
-n = 8; % just an example
+func = @(x) cos(x.^2);
+a = 1; % start_value = 0;
+b = 2; % stop_value = 1;
+n = 10; % just an example
 h = (b-a)/n;
-Sh_part_1 = h/3 * (f(0) + f(1));
+
+x = 1:h:2;
+f = func(x); % f has 11 elements
+
+% Simpsons
+Sh_part_1 = h/3 * (f(1) + f(11));
 Sh_part_2 = 0;
 for j = 1:n/2
-    x_index = 2*j-1;
+    x_index = 2*j; % 2j-1 for zero index
     Sh_part_2 = Sh_part_2 + 4*h/3 * f(x_index);
 end
 
 Sh_part_3 = 0;
 for j = 1:n/2-1
-    x_index = 2*j;
+    x_index = 2*j+1; % 2j for zero index
     Sh_part_3 = Sh_part_3 + 2*h/3 * f(x_index);
 end
 
 Sh = Sh_part_1 + Sh_part_2 + Sh_part_3;
-disp(Sh)
+
+% Trapetsregeln
+Th = 0;
+for j = 1:n % 10 steps
+    top = func(a + (j-1)*h);
+    bot = func(a + j*h);
+    height = h;
+    Th = Th + (top+bot)*height/2;
+end
+    
